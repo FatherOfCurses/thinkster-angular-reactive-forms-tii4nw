8eb7bc7480a6db601ef4b9f69e82a21f8b7c2be9
FatherOfCurses <colin.principe@gmail.com>
2021-04-15 18:13:03 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms'
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit {
+  locations: string[] = ['Downtown', 'S. County', 'Lakeside'];
+  volunteerForm: FormGroup;
+
+  constructor(private fb: FormBuilder){ }
+
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.volunteerForm = this.fb.group({
+      
+    })
+  }
+}
